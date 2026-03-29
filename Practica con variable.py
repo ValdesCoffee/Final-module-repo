16d269340eb970070e15661a46ff9bc118fcc04d
ValdesCoffe <valdes.2022806@ensdbexcelencia.edu.co>
2026-03-29 18:24:39 -0500

    Agrego Practica con variable.py

---
+++ Practica con variable.py
@@ -0,0 +1,32 @@
+# numero = int(input("Ingresa un número: "))
+# # Primero defines una variable tipo entero corto
+# if numero % 2 == 0:
+#     print("El número es Par")
+'''
+Utilizas una condicion con la variable antes definida
+utilizas la modulo que busca el residuo de esa variable
+''' 
+# # y si su residuo es = 0 es PAR
+# else:
+#     print("El número es Impar")
+# # y si deja residuo es IMPAR
+#-----------------------------------------
+'''Construir un programa que reciba dos numeros enteros y calcule el resultado de elevar el 1° numero leido a la potencia representada por el 2° numero leido.
+Este es un ejercicio del libro de "introduccion a la programacion con python" de Raul omar trejos y luis muñoz
+'''
+# varEnTERO=int(input("Ingresa tu numero :3 :"))
+# varEnTERO2=int(input("Ingresa la potencia a la que quieres elevar tu numero :3 :"))
+
+# print("El resultado va elvarse ☆*: .｡. o(≧▽≦)o .｡.:*☆")
+# resultado=varEnTERO**varEnTERO2
+# '''Luego de haber hecho la declaracion de variables y la operacion se va escribir un numero'''
+# #Realiza un print para mostrar el resultado
+# print("este es el resultado de tu digitacion :3",resultado)
+#___________________________________________________________
+#Construir un programa que reciba un numero entero y obtenga su mitad entera.
+# '''Este es un ejercicio del libro de "introduccion a la programacion con python" de Raul omar trejos y luis muñoz'''
+# varENTERO=int(input("ingresa un numero que no tenga decimales (❁´◡`❁)"))
+# resultado = varENTERO//2
+# Mitadxnumero = resultado
+# '''luego de haber hecho esto aparecera un numero'''
+# print("mira aca esta tu numero",Mitadxnumero)
